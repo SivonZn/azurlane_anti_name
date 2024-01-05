3b35e7d2e10f6ae72b69bf244b2c24a207a0188b
SivonZn <79399171+SivonZn@users.noreply.github.com>
2024-01-05 14:03:13 +0800

    Create patch_huawei.sh

---
+++ sh/patch_huawei.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# Download Azur Lane
+# Download Azur Lane
+download_azurlane () {
+    if [ ! -f "AzurLane.apk" ]; then
+    url="https://store-drcn.hispace.dbankcloud.cn/dl/appdl/application/apk/7c/7c707aebf7e04e1691164176fc4b781b/com.bilibili.blhx.huawei.2312211917.apk"
+    # curl -o blhx.apk -L $url
+    axel -n 16 -k -o blhx.apk $url
+    fi
+}
+
+if [ ! -f "AzurLane.apk" ]; then
+    echo "Get Azur Lane apk"
+    download_azurlane
+    mv *.apk "AzurLane.apk"
+fi
+
+
+echo "Decompile Azur Lane apk"
+java -jar apktool.jar  -f d AzurLane.apk
+
+echo "Copy libs"
+cp -r libs/. AzurLane/lib/
+
+echo "Patching Azur Lane"
+oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+
+oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+
+
+echo "Build Patched Azur Lane apk"
+java -jar apktool.jar  -f b AzurLane -o AzurLane.patched.apk
+
+echo "Set Github Release version"
+
+echo "PERSEUS_VERSION=$(echo XiaoMi)" >> $GITHUB_ENV
+
+mkdir -p build
+mv *.patched.apk ./build/
+find . -name "*.apk" -print
